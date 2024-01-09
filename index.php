5a99da39841655f45e3f17eeda512342efbf3771
Abyss <112321554+Arctic-Void@users.noreply.github.com>
2024-01-09 13:52:42 +0100

    edited title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Slider Example</title>
+  <title>Solvware Proefopdracht</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 
